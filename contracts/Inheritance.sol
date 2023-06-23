88b8b2f781af7444a99b6e29e19ac9d22093e622
Raffa Bux <raffaelebussolotto@gmail.com>
2023-06-23 11:13:16 +0200

    implemented heir set method

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -1,103 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19 <0.9.0;
 
-// struct Degree {
-// 	string degreeType;
-// 	string name;
-// }
-
-// struct CredentialSubject {
-// 	string id;
-// 	Degree degree;
-// }
-
-struct Proof {
-	string proofType;
-	string creationDate;
-	string verificationMethod;
-	string proofPurpose;
-	string proofValue;
-}
-
-struct VerifiableCredential {
-	string[] context;
-	string id;
-	string[] credentialTypes;
-	string issuer;
-	string issuanceDate;
-	// CredentialSubject credentialSubject;
-	Proof proof;
-}
-
-// → ?? non mi ricordo di preciso chi è ç-ç
-// → Issuer è l'inheritanceOwner quando viene invoato il contratto
-// 		contratto la prima volta ma che succede quando 
-//		inheritanceOwner muore???
+// Issuer è l'inheritanceOwner (msg.sender quando viene deployato il contratto)
 struct Issuer {
 	address issuerAddress;
-	// string issuerDid;
-	// string signature;
 }
 
-// → per il momento ogni account con amount != 0 è attivo
-struct Account {
-	uint accountId;
-	address payable accountAddress;
-	uint accountAmount;
-	// bool active;
-}
-
-// → in questa maniera se l'utente perde la chiave privata
-//		son cazzi perché gli inculano la sua fetta di eredità
+// atm se l'utente perde la chiave privata son cazzi perché gli inculano la sua fetta di eredità
 struct Heir {
-	uint heirId;
 	string heirDid;
 	bool delegated;
-	// bool active;
-	mapping(uint => Account) wallet;
+	uint addressCounter;	
+	mapping(uint => Account) wallet;	// uint (from 0 to addressCounter - 1) → Account (that could be active or not)
+	bool active;
+}
+
+// atm ogni account con amount != 0 è attivo
+struct Account {
+	string accountId;	// #identifier
+	address payable accountAddress;	// 0x0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+	uint percentage;
+	bool active;
 }
 
 contract Inheritance {
 
-	VerifiableCredential private vC;
 	Issuer private inheritanceOwner;
 	
-	// did → Heir
-	mapping(string => Heir) private heirMap; 
+	// uint (from 0 to heirCounter - 1) → Heir (that could be active or not)
+	uint heirCounter;
+	mapping(uint => Heir) private heirMap; 
 
 	constructor() {	
 		inheritanceOwner = Issuer(msg.sender);
+		heirCounter = 0;
 	}
 
+	// validità dei dati passati controllata fuori contratto
+	// _heirDid + _accountIdentifierList[i] → _addressList[i]
 	function setHeir(
-		uint _heirId,
-		string memory _heirDid,
-		bool _delegation
+		string memory _heirDid,	// did:ssi-cot-eth:chainId:xxxxxxx
+		bool _delegation,
+		address payable[] memory _addressList,	// 0x0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+		string[] memory _accountIdentifierList,	// #identifier
+		uint[] memory _percentageList,
+		bool _heirActivation
 	) public returns (bool) {
-		require(msg.sender == inheritanceOwner.issuerAddress, 'The setter is not the issuer!');
+		require(msg.sender == inheritanceOwner.issuerAddress, 'The setter is not the issuer (inheritance owner)!');
 
-		Heir storage newHeir = heirMap[_heirDid];
-		newHeir.heirId = _heirId;
+		Heir storage newHeir = heirMap[heirCounter++];
+
 		newHeir.heirDid = _heirDid;
 		newHeir.delegated = _delegation;
+		newHeir.addressCounter = 0;
+
+		require(
+			_addressList.length == _percentageList.length && _addressList.length == _accountIdentifierList.length,
+			'Account identifiers list length and percentage list length dont match!'
+		);
+
+		for(uint i = 0; i < _accountIdentifierList.length; i++) {
+			newHeir.wallet[newHeir.addressCounter++] = Account(_accountIdentifierList[i], _addressList[i], _percentageList[i], _heirActivation);
+		}
 
 		return true;
 	}
-
-	function setHeirAddresses(
-		uint index,
-		string memory _heirDid,
-		uint _accountId,
-		address payable _accountAddress,
-		uint _accountAmount
-	) public returns (bool) {
-		require(msg.sender == inheritanceOwner.issuerAddress, 'The setter is not the issuer!');
-		
-		heirMap[_heirDid].wallet[index] = Account(_accountId, _accountAddress, _accountAmount);
-
-		return true;
-	}
-
-	// function modifyHeir() public {}	//TODO
 	
 }
